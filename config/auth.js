712954efe111bc5f9263521b5e97a406a7fd522e
Justice Eziefule <eziefulejustice@gmail.com>
2019-08-14 00:51:44 +0100

    landing page create

--- config/auth.js
+++ config/auth.js
@@ -1,8 +1,8 @@
 module.exports = {
     'facebookAuth' : {
-        'clientID': '376887439667326',
-        'clientSecret': '167e9dd6e670fa78590bc06e65f7e0d1',
-        'callbackURL': 'http://localhost:3000/auth/facebook/callback',
+        'clientID': '',
+        'clientSecret': '',
+        'callbackURL': '',
         'profileFields': ['id', 'email', 'name']
     }
 }
--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,3 +1,17 @@
+// window pre loading animation
+$(window).load(function() {
+	// Animate loader off screen
+	$(".se-pre-con").fadeOut("slow");;
+});
+
+//============================================================================
+//                                 SERVICES SECTION
+//============================================================================
+$(function() {
+    // animate on scroll
+    new WOW().init();
+});
+
 // login and register navbar
 function onClickMenu(){
 	document.getElementById("menu").classList.toggle("change");
